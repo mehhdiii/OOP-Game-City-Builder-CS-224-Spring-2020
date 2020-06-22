ea1e3f4307e336a22745e9d5e367ed36fc165da9
mehhdiii <60067141+mehhdiii@users.noreply.github.com>
2020-06-23 02:09:59 +0500

    🐱‍👤Upper grid done
    Co-Authored-By: hamzanaqvi1 <hamzanaqvi1@users.noreply.github.com>

--- map.hpp
+++ map.hpp
@@ -2,6 +2,8 @@
 #include "unit.hpp"
 #include<string>
 #include<iostream>
+#include<vector>
+#include<cmath>
 #pragma once
 
 class Map: public Unit{
@@ -14,6 +16,10 @@ class Map: public Unit{
         std::string theme;
         const int MAP_WIDTH = 4964;
         const int MAP_HEIGHT = 2490;
+        std::vector<int> grid_x; 
+        std::vector<int> grid_y;
+        int GRID_WIDTH =2998 -  2498;
+        int GRID_HEIGHT = 626 - 375;
         const int SCROLL_SPEED = 50;
         //vector to contain all different types of maps 
         // std::vector<SDL_Texture *> all_maps;
@@ -26,4 +32,7 @@ class Map: public Unit{
 
         void scroll(bool, bool, bool, bool);
         bool scrolling_flag(bool, bool, bool, bool); // checks if scrolling is possible
+
+        void add_grid_point(); //add the grid point into the map array
+        void check_grid(int&, int&);
 };
