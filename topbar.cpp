9226b6f9ff2ff37efed60fe313b0d0b46a610145
Ali-Rizvi-1 <63605517+Ali-Rizvi-1@users.noreply.github.com>
2020-06-23 17:05:21 +0500

    last_update_text

--- topbar.cpp
+++ topbar.cpp
@@ -70,6 +70,7 @@ void Topbar::draw(SDL_Renderer * gRenderer){ //, int & main_cash, int & XP_level
 }
 
 void Topbar::draw_modified(SDL_Renderer* gRenderer, int & main_cash, int & XP_level, int & P_level, int & green_energy){
+    cash = main_cash;
     float top_cash = 10000.0;
     float top_xp_level = 250.0;
     float top_p_level = 4.0;
@@ -217,7 +218,7 @@ void Topbar::setRect(){
     xplevel_mover.w = width;
     xplevel_mover.h =height;
 
-    oxygenlevel_mover.x = 1079+35;
+    oxygenlevel_mover.x = 1079+45;
     oxygenlevel_mover.y = 7;
     oxygenlevel_mover.w = width;
     oxygenlevel_mover.h = height;
@@ -225,14 +226,10 @@ void Topbar::setRect(){
     //setting coordinates of text object
     text_objects[0]->setCoordinates(1079 -30, oxygenlevel_mover.y); //+oxygenlevel_mover.w
 
-    text_objects[1]->setCoordinates(826 -20, xplevel_mover.y );//+xplevel_mover.w
+    text_objects[1]->setCoordinates(826 -40, xplevel_mover.y );//+xplevel_mover.w
 
-    text_objects[2]->setCoordinates(519-20, greenenergy_mover.y);//+greenenergy_mover.w
-    text_objects[3]->setCoordinates(180, cash_mover.y ); //+cash_mover.w
-
-    
-
-
+    text_objects[2]->setCoordinates(519-35, greenenergy_mover.y);//+greenenergy_mover.w
+    text_objects[3]->setCoordinates(180-40, cash_mover.y ); //+cash_mover.w  
 }
 
 
